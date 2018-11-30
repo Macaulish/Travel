7895f9cabcbbd3e6492c7799f31d85f9e98a6843
Macaulish <1044264503@qq.com>
2018-11-30 15:48:38 +0800

    modified sth

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>home</div>
+    <router-link to = "/">跳转lust</router-link>
+  </div>
+</template>
+<script>
+export default{
+  name: 'home'
+}
+</script>
+
+<style>
+
+</style>
