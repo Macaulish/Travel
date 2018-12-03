b7f9d1d2b8706f8557638da745d55f4ad9f0b825
Macaulish <1044264503@qq.com>
2018-12-03 19:56:23 +0800

    add recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <!-- <div class="item-img-wrapper"> -->
+        <img class="item-img" :src="item.imgUrl" alt="">
+        <!-- </div> -->
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '001',
+        title: 'title',
+        imgUrl: 'http://img1.qunarzz.com/sight/p39/201211/00/86d1e4c007f206c493835fbb.jpg_200x200_e5841767.jpg',
+        desc: 'desc'
+      }, {
+        id: '002',
+        title: 'title',
+        imgUrl: 'http://img1.qunarzz.com/sight/p39/201211/00/86d1e4c007f206c493835fbb.jpg_200x200_e5841767.jpg',
+        desc: 'desc'
+      }, {
+        id: '003',
+        title: 'title',
+        imgUrl: 'http://img1.qunarzz.com/sight/p39/201211/00/86d1e4c007f206c493835fbb.jpg_200x200_e5841767.jpg',
+        desc: 'desc'
+      }, {
+        id: '004',
+        title: 'title',
+        imgUrl: 'http://img1.qunarzz.com/sight/p39/201211/00/86d1e4c007f206c493835fbb.jpg_200x200_e5841767.jpg',
+        desc: 'desc'
+      }]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item
+    overflow: hidden
+    display: flex
+    height: 1.9rem
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color: #ccc
+        ellipsis()
+      .item-button
+        line-height:.44rem
+        margin-top: .16rem
+        color: #fff
+        background: #ff9300
+        padding: 0 .2rem
+        border-radius: .06rem
+</style>
