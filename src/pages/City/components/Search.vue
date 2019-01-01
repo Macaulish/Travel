a5b269a09a7bacfde82f524576d4c840f49736a8
Macaulish <1044264503@qq.com>
2019-01-01 15:28:09 +0800

    add search to city

---
+++ src/pages/City/components/Search.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="search">
+		<input type="text" class="search-input" placeholder="输入城市名或拼音">
+		<!--<i class = "iconfont search-icon">&#xe632;</i>-->
+	</div>
+</template>
+<script>
+export default {
+  name: 'citySearch'
+}
+</script>
+<style lang="stylus" scoped>
+	@import '~styles/varibles.styl'
+	.search
+		height: .72rem
+		background: $bgColor
+		padding: .1rem
+		.search-input
+			box-sizing: border-box
+			padding: 0 .1rem
+			height: .62rem
+			width: 100%
+			line-height: .62rem
+			text-align: center
+			border-radius: .1rem
+			color: #666
+</style>
