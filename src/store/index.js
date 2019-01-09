c00b3f75378edef5ea203c911347a9d7a08c01d6
Macaulish <1044264503@qq.com>
2019-01-09 19:17:43 +0800

    test

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-// import store from '@/store'
+import state from './state'
+import mutations from './mutations'
 
 Vue.use(Vuex)
+
 export default new Vuex.Store({
-  state: {
-    city: '北555京'
-  },
-  // actions: {
-  //   changeCity (ctx, city) {
-  //     // console.log(city)
-  //     ctx.commit('changeCity', city)
-  //   }
-  // },
-  mutations: {
-    changeCity (state, city) {
-      state.city = city
-    }
-  }
+  state,
+  mutations: mutations // 键值都一样 就可以写一个
 })
